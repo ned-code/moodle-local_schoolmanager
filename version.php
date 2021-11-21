d155f9e81492e6ecd1b2ead4f0843cb08cc899f8
Michael Gardener <mgardener@cissq.com>
2021-11-21 10:55:56 -0500

    Show number of certificates

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021111700;
+$plugin->version  = 2021112100;
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->component = 'local_schoolmanager';
