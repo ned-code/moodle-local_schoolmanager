59aa1c85bc2644ac5ae5c93667f2469c4b9d17d1
Michael Gardener <mgardener@cissq.com>
2024-06-20 00:33:24 +0300

    String update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024061900;
+$plugin->version  = 2024062000;
 $plugin->requires = 2017051500.00;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->component = 'local_schoolmanager';
